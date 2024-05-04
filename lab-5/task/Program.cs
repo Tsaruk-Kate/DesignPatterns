8a8af1a1afb2591e488c60bd024c1a554501f918
Kate Tsaruk <ipz224_tska@student.ztu.edu.ua>
2024-05-05 01:02:05 +0300

    State

--- lab-5/task/Program.cs
+++ lab-5/task/Program.cs
@@ -1,23 +1,93 @@
 ﻿using System;
 using System.Collections.Generic;
 
-class LightNode
+// Інтерфейс для стейт
+interface IHtmlState
 {
-    public virtual string GetOuterHtml() { return ""; }
-    public virtual string GetInnerHtml() { return ""; }
+    void RenderHtml(LightNode node);
+    void SwitchToViewMode(LightNode node);
+    void SwitchToEditMode(LightNode node);
+}
+
+// Конкретний стан «Режим перегляду»
+class ViewMode : IHtmlState
+{
+    public void RenderHtml(LightNode node)
+    {
+        Console.WriteLine(node.GetOuterHtml());
+    }
+
+    public void SwitchToViewMode(LightNode node)
+    {
+        // Вже в режимі перегляду
+    }
+
+    public void SwitchToEditMode(LightNode node)
+    {
+        node.SetEditMode();
+    }
+}
+
+// Конкретний стан «Режим редагування»
+class EditMode : IHtmlState
+{
+    public void RenderHtml(LightNode node)
+    {
+        Console.WriteLine(node.GetInnerHtml());
+    }
+
+    public void SwitchToViewMode(LightNode node)
+    {
+        node.SetViewMode();
+    }
+
+    public void SwitchToEditMode(LightNode node)
+    {
+        // Вже в режимі редагування
+    }
+}
+
+abstract class LightNode
+{
+    protected IHtmlState _state;
+
+    public void SetState(IHtmlState state)
+    {
+        _state = state;
+    }
+
+    public abstract string GetOuterHtml();
+    public abstract string GetInnerHtml();
+
+    public virtual void SwitchToViewMode()
+    {
+        _state.SwitchToViewMode(this);
+    }
+
+    public virtual void SwitchToEditMode()
+    {
+        _state.SwitchToEditMode(this);
+    }
+
+    public virtual void SetEditMode() { }
+    public virtual void SetViewMode() { }
 }
 
 class LightTextNode : LightNode
 {
     private string _text;
+
     public LightTextNode(string text)
     {
         _text = text;
+        SetState(new ViewMode());
     }
+
     public override string GetOuterHtml()
     {
         return _text;
     }
+
     public override string GetInnerHtml()
     {
         return _text;
@@ -31,6 +101,7 @@ class LightElementNode : LightNode
     private string _closingType;
     private List<LightNode> _children;
     private List<string> _cssClasses;
+    private Dictionary<string, string> _attributes;
 
     public LightElementNode(string tagName, string displayType, string closingType, List<string> cssClasses)
     {
@@ -39,21 +110,38 @@ class LightElementNode : LightNode
         _closingType = closingType;
         _cssClasses = cssClasses;
         _children = new List<LightNode>();
+        _attributes = new Dictionary<string, string>();
+        SetState(new ViewMode());
     }
 
     public void AddChild(LightNode node)
     {
-        _children.Add(node);
+        if (node != null)
+            _children.Add(node);
     }
 
-    public void RemoveChild(LightNode node)
+    // Методи додавання та видалення атрибутів
+    public void AddAttribute(string key, string value)
     {
-        _children.Remove(node);
+        _attributes[key] = value;
+    }
+
+    public void RemoveAttribute(string key)
+    {
+        _attributes.Remove(key);
     }
 
     public override string GetOuterHtml()
     {
-        string result = $"<{_tagName} class=\"{string.Join(" ", _cssClasses)}\" display=\"{_displayType}\" closing=\"{_closingType}\">\n";
+        string result = $"<{_tagName} class=\"{string.Join(" ", _cssClasses)}\" display=\"{_displayType}\" closing=\"{_closingType}\"";
+
+        // Додавання атрибутів до HTML-виводу
+        foreach (var attribute in _attributes)
+        {
+            result += $" {attribute.Key}=\"{attribute.Value}\"";
+        }
+
+        result += ">\n";
         foreach (var child in _children)
         {
             result += $"\t{child.GetOuterHtml()}\n";
@@ -74,94 +162,129 @@ class LightElementNode : LightNode
         }
         return result;
     }
+
+    public override void SetEditMode()
+    {
+        SetState(new EditMode());
+        foreach (var child in _children)
+        {
+            child.SetEditMode();
+        }
+    }
+
+    public override void SetViewMode()
+    {
+        SetState(new ViewMode());
+        foreach (var child in _children)
+        {
+            child.SetViewMode();
+        }
+    }
 }
-// Інтерфейс команди
-interface ICommand
+
+// Контекстний клас
+class HtmlContext
 {
-    void Execute(); // Метод виконання команди
-}
+    private IHtmlState _state;
 
-// Команда для додавання дочірнього вузла
-class AddChildCommand : ICommand
-{
-    private LightElementNode _parent;
-    private LightNode _child;
-
-    public AddChildCommand(LightElementNode parent, LightNode child)
+    public HtmlContext()
     {
-        _parent = parent;
-        _child = child;
+        // Початковий стан - режим перегляду
+        TransitionTo(new ViewMode());
     }
 
-    public void Execute()
+    // Перехідні стани
+    public void TransitionTo(IHtmlState state)
     {
-        _parent.AddChild(_child);
-    }
-}
-
-// Команда для видалення дочірнього вузла
-class RemoveChildCommand : ICommand
-{
-    private LightElementNode _parent;
-    private LightNode _child;
-
-    public RemoveChildCommand(LightElementNode parent, LightNode child)
-    {
-        _parent = parent;
-        _child = child;
+        Console.WriteLine($"Context: Transition to {state.GetType().Name}.");
+        _state = state;
     }
 
-    public void Execute()
+    // Метод рендерингу HTML
+    public void RenderHtml(LightNode node)
     {
-        _parent.RemoveChild(_child);
+        _state.RenderHtml(node);
     }
-}
 
-// Виконавець команд
-class CommandInvoker
-{
-    private List<ICommand> _commands = new List<ICommand>();
-
-    public void StoreAndExecute(ICommand command)
+    // Методи додавання та видалення атрибутів у режимі редагування
+    public void AddAttribute(LightElementNode node, string key, string value)
     {
-        _commands.Add(command);
-        command.Execute();
+        if (_state is EditMode)
+        {
+            node.AddAttribute(key, value);
+        }
+        else
+        {
+            Console.WriteLine("Attributes can only be added in edit mode.");
+        }
+    }
+
+    public void RemoveAttribute(LightElementNode node, string key)
+    {
+        if (_state is EditMode)
+        {
+            node.RemoveAttribute(key);
+        }
+        else
+        {
+            Console.WriteLine("Attributes can only be removed in edit mode.");
+        }
     }
 }
 
-// Головний клас програми
 class Program
 {
     static void Main(string[] args)
     {
-        CommandInvoker invoker = new CommandInvoker();
+        HtmlContext context = new HtmlContext();
 
-        // Створюємо елементи дерева HTML
         LightElementNode header = new LightElementNode("h1", "block", "closing", new List<string>());
         LightTextNode headerText = new LightTextNode("Welcome to my page!");
-        invoker.StoreAndExecute(new AddChildCommand(header, headerText));
+        header.AddChild(headerText);
 
         LightElementNode table = new LightElementNode("table", "block", "closing", new List<string> { "styled-table" });
-        invoker.StoreAndExecute(new AddChildCommand(header, table));
 
         LightElementNode tableRow1 = new LightElementNode("tr", "block", "closing", new List<string>());
-        invoker.StoreAndExecute(new AddChildCommand(table, tableRow1));
+        table.AddChild(tableRow1);
 
         LightElementNode tableData1 = new LightElementNode("td", "inline", "closing", new List<string>());
         LightTextNode dataText1 = new LightTextNode("Cell 1");
-        invoker.StoreAndExecute(new AddChildCommand(tableRow1, tableData1));
-        invoker.StoreAndExecute(new AddChildCommand(tableData1, dataText1));
+        tableData1.AddChild(dataText1);
+        tableRow1.AddChild(tableData1);
 
         LightElementNode tableData2 = new LightElementNode("td", "inline", "closing", new List<string>());
         LightTextNode dataText2 = new LightTextNode("Cell 2");
-        invoker.StoreAndExecute(new AddChildCommand(tableRow1, tableData2));
-        invoker.StoreAndExecute(new AddChildCommand(tableData2, dataText2));
+        tableData2.AddChild(dataText2);
+        tableRow1.AddChild(tableData2);
 
-        // Видаляємо заголовок
-        invoker.StoreAndExecute(new RemoveChildCommand(header, headerText));
+        // Перегляд HTML
+        context.RenderHtml(header);
+        context.RenderHtml(table);
 
-        // Виводимо HTML
-        Console.WriteLine(header.GetOuterHtml());
-        Console.WriteLine(table.GetOuterHtml());
+        // Перехід до режиму редагування
+        context.TransitionTo(new EditMode());
+
+        // Редагування
+        context.RenderHtml(header);
+        context.RenderHtml(table);
+
+        // Перехід до режиму перегляду
+        context.TransitionTo(new ViewMode());
+
+        // Повторний перегляд HTML
+        context.RenderHtml(header);
+        context.RenderHtml(table);
+
+        // Зміна режиму між елементами
+        table.SetEditMode();
+
+        // Додавання та видалення атрибутів
+        context.AddAttribute(table, "border", "1");
+        context.AddAttribute(table, "cellpadding", "5");
+        context.RemoveAttribute(table, "class");
+
+        // Повторний перегляд HTML
+        context.RenderHtml(header);
+        context.RenderHtml(table);
     }
 }
