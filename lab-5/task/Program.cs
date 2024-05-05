243468ed5c82f95533444f1ecad2cba160d7fe5c
Kate Tsaruk <ipz224_tska@student.ztu.edu.ua>
2024-05-05 21:05:02 +0300

    Visitor

--- lab-5/task/Program.cs
+++ lab-5/task/Program.cs
@@ -1,97 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
-abstract class NodeLifecycleHooks
+// Базовий клас для відвідувача
+interface IVisitor
 {
-    // Абстрактні методи для кроків життєвого циклу
-    public abstract void OnCreated();
-    public abstract void OnInserted();
-    public abstract void OnRemoved();
-    public abstract void OnStylesApplied();
-    public abstract void OnClassListApplied();
-    public abstract void OnTextRendered();
-
-    // Шаблонний метод для виклику кроків життєвого циклу
-    public void RunLifecycleHooks()
+    void VisitElementNode(LightElementNode elementNode);
+    void VisitTextNode(LightTextNode textNode);
+    void VisitListElementNode(LightListElementNode listElementNode);
+    void VisitListItemNode(LightListItemNode listItemNode);
+    void VisitTextInputNode(LightTextInputNode textInputNode);
+    void VisitButtonNode(LightButtonNode buttonNode);
+    void VisitSelectNode(LightSelectNode selectNode);
+}
+// Реалізація відвідувача для виводу HTML
+class HtmlVisitor : IVisitor
+{
+    public void VisitElementNode(LightElementNode elementNode)
     {
-        OnCreated();
-        OnInserted();
-        OnRemoved();
-        OnStylesApplied();
-        OnClassListApplied();
-        OnTextRendered();
+        Console.WriteLine(elementNode.GetOuterHtml());
+    }
+    public void VisitTextNode(LightTextNode textNode)
+    {
+        Console.WriteLine(textNode.GetOuterHtml());
+    }
+    public void VisitListElementNode(LightListElementNode listElementNode)
+    {
+        Console.WriteLine(listElementNode.GetOuterHtml());
+    }
+    public void VisitListItemNode(LightListItemNode listItemNode)
+    {
+        Console.WriteLine(listItemNode.GetOuterHtml());
+    }
+    public void VisitTextInputNode(LightTextInputNode textInputNode)
+    {
+        Console.WriteLine(textInputNode.GetOuterHtml());
+    }
+    public void VisitButtonNode(LightButtonNode buttonNode)
+    {
+        Console.WriteLine(buttonNode.GetOuterHtml());
+    }
+    public void VisitSelectNode(LightSelectNode selectNode)
+    {
+        Console.WriteLine(selectNode.GetOuterHtml());
     }
 }
-
-class ElementLifecycleHooks : NodeLifecycleHooks
+// Базовий клас для вузла
+abstract class LightNode
 {
-    public override void OnCreated()
-    {
-        Console.WriteLine("Element created.");
-    }
-
-    public override void OnInserted()
-    {
-        Console.WriteLine("Element inserted.");
-    }
-
-    public override void OnRemoved()
-    {
-        Console.WriteLine("Element removed.");
-    }
-
-    public override void OnStylesApplied()
-    {
-        Console.WriteLine("Styles applied to element.");
-    }
-
-    public override void OnClassListApplied()
-    {
-        Console.WriteLine("Class list applied to element.");
-    }
-
-    public override void OnTextRendered()
-    {
-        Console.WriteLine("Text rendered inside element.");
-    }
+    public abstract string GetOuterHtml();
+    public abstract string GetInnerHtml();
+    public abstract void Accept(IVisitor visitor);
 }
-
-class LightNode
-{
-    public virtual string GetOuterHtml() { return ""; }
-    public virtual string GetInnerHtml() { return ""; }
-
-    // Додано метод для виклику хуків життєвого циклу
-    public virtual void RunLifecycleHooks() { }
-}
-
+// Клас для текстового вузла
 class LightTextNode : LightNode
 {
     private string _text;
-    private NodeLifecycleHooks _lifecycleHooks;
-
     public LightTextNode(string text)
     {
         _text = text;
-        _lifecycleHooks = new ElementLifecycleHooks();
     }
-
     public override string GetOuterHtml()
     {
         return _text;
     }
-
     public override string GetInnerHtml()
     {
         return _text;
     }
-
-    public override void RunLifecycleHooks()
+    public override void Accept(IVisitor visitor)
     {
-        _lifecycleHooks.RunLifecycleHooks();
+        visitor.VisitTextNode(this);
     }
 }
-
+// Розширений клас для елемента вузла з атрибутами
 class LightElementNode : LightNode
 {
     private string _tagName;
@@ -99,8 +81,7 @@ class LightElementNode : LightNode
     private string _closingType;
     private List<LightNode> _children;
     private List<string> _cssClasses;
-    private NodeLifecycleHooks _lifecycleHooks;
-
+    private Dictionary<string, string> _attributes;
     public LightElementNode(string tagName, string displayType, string closingType, List<string> cssClasses)
     {
         _tagName = tagName;
@@ -108,18 +89,16 @@ class LightElementNode : LightNode
         _closingType = closingType;
         _cssClasses = cssClasses;
         _children = new List<LightNode>();
-        _lifecycleHooks = new ElementLifecycleHooks();
+        _attributes = new Dictionary<string, string>();
     }
-
-    public void AddChild(LightNode node)
+    public void AddAttribute(string attributeName, string attributeValue)
     {
-        _children.Add(node);
+        _attributes[attributeName] = attributeValue;
     }
-
     public override string GetOuterHtml()
     {
-        RunLifecycleHooks(); // Виклик хуків перед отриманням HTML
-        string result = $"<{_tagName} class=\"{string.Join(" ", _cssClasses)}\" display=\"{_displayType}\" closing=\"{_closingType}\">\n";
+        string attributes = string.Join(" ", _attributes.Select(kv => $"{kv.Key}=\"{kv.Value}\"")); // Генерація рядка атрибутів
+        string result = $"<{_tagName} class=\"{string.Join(" ", _cssClasses)}\" display=\"{_displayType}\" closing=\"{_closingType}\" {attributes}>\n";
         foreach (var child in _children)
         {
             result += $"\t{child.GetOuterHtml()}\n";
@@ -130,7 +109,6 @@ class LightElementNode : LightNode
         }
         return result;
     }
-
     public override string GetInnerHtml()
     {
         string result = "";
@@ -140,72 +118,179 @@ class LightElementNode : LightNode
         }
         return result;
     }
-
-    public override void RunLifecycleHooks()
+    public void AddChild(LightNode node)
     {
-        _lifecycleHooks.RunLifecycleHooks();
+        _children.Add(node);
+    }
+    public override void Accept(IVisitor visitor)
+    {
+        visitor.VisitElementNode(this);
     }
 }
-
-class TextNodeLifecycleHooks : NodeLifecycleHooks
+// Клас для елемента списку
+class LightListItemNode : LightNode
 {
-    public override void OnCreated()
+    private List<LightNode> _children;
+    public LightListItemNode()
     {
-        Console.WriteLine("Text node created.");
+        _children = new List<LightNode>();
     }
-
-    public override void OnInserted()
+    public override string GetOuterHtml()
     {
-        Console.WriteLine("Text node inserted.");
+        string result = "<li>\n";
+        foreach (var child in _children)
+        {
+            result += $"\t{child.GetOuterHtml()}\n";
+        }
+        result += "</li>";
+        return result;
     }
-
-    public override void OnRemoved()
+    public override string GetInnerHtml()
     {
-        Console.WriteLine("Text node removed.");
+        string result = "";
+        foreach (var child in _children)
+        {
+            result += child.GetInnerHtml();
+        }
+        return result;
     }
-
-    public override void OnStylesApplied()
+    public void AddChild(LightNode node)
     {
-        Console.WriteLine("Styles applied to text node.");
+        _children.Add(node);
     }
-
-    public override void OnClassListApplied()
+    public override void Accept(IVisitor visitor)
     {
-        Console.WriteLine("Class list applied to text node.");
-    }
-
-    public override void OnTextRendered()
-    {
-        Console.WriteLine("Text node rendered.");
+        visitor.VisitListItemNode(this);
     }
 }
-
+// Клас для списку (ul або ol)
+class LightListElementNode : LightNode
+{
+    private List<LightNode> _children;
+    private string _listType;
+    public LightListElementNode(string listType)
+    {
+        _listType = listType;
+        _children = new List<LightNode>();
+    }
+    public override string GetOuterHtml()
+    {
+        string result = $"<{_listType}>\n";
+        foreach (var child in _children)
+        {
+            result += $"\t{child.GetOuterHtml()}\n";
+        }
+        result += $"</{_listType}>";
+        return result;
+    }
+    public override string GetInnerHtml()
+    {
+        string result = "";
+        foreach (var child in _children)
+        {
+            result += child.GetInnerHtml();
+        }
+        return result;
+    }
+    public void AddChild(LightNode node)
+    {
+        _children.Add(node);
+    }
+    public override void Accept(IVisitor visitor)
+    {
+        visitor.VisitListElementNode(this);
+    }
+}
+// Клас для елемента форми - текстового поля
+class LightTextInputNode : LightNode
+{
+    private string _name;
+    public LightTextInputNode(string name)
+    {
+        _name = name;
+    }
+    public override string GetOuterHtml()
+    {
+        return $"<input type=\"text\" name=\"{_name}\">";
+    }
+    public override string GetInnerHtml()
+    {
+        return "";
+    }
+    public override void Accept(IVisitor visitor)
+    {
+        visitor.VisitTextInputNode(this);
+    }
+}
+// Клас для елемента форми - кнопки
+class LightButtonNode : LightNode
+{
+    private string _text;
+    public LightButtonNode(string text)
+    {
+        _text = text;
+    }
+    public override string GetOuterHtml()
+    {
+        return $"<button>{_text}</button>";
+    }
+    public override string GetInnerHtml()
+    {
+        return "";
+    }
+    public override void Accept(IVisitor visitor)
+    {
+        visitor.VisitButtonNode(this);
+    }
+}
+// Клас для елемента форми - випадаючого списку
+class LightSelectNode : LightNode
+{
+    private List<string> _options;
+    public LightSelectNode(List<string> options)
+    {
+        _options = options;
+    }
+    public override string GetOuterHtml()
+    {
+        string optionsHtml = "";
+        foreach (var option in _options)
+        {
+            optionsHtml += $"<option>{option}</option>";
+        }
+        return $"<select>{optionsHtml}</select>";
+    }
+    public override string GetInnerHtml()
+    {
+        return "";
+    }
+    public override void Accept(IVisitor visitor)
+    {
+        visitor.VisitSelectNode(this);
+    }
+}
+// Клас програми
 class Program
 {
     static void Main(string[] args)
     {
+        // Створюємо дерево HTML
         LightElementNode header = new LightElementNode("h1", "block", "closing", new List<string>());
+        header.AddAttribute("id", "main-header");
         LightTextNode headerText = new LightTextNode("Welcome to my page!");
         header.AddChild(headerText);
-
-        LightElementNode table = new LightElementNode("table", "block", "closing", new List<string> { "styled-table" });
-
-        LightElementNode tableRow1 = new LightElementNode("tr", "block", "closing", new List<string>());
-        table.AddChild(tableRow1);
-
-        LightElementNode tableData1 = new LightElementNode("td", "inline", "closing", new List<string>());
-        LightTextNode dataText1 = new LightTextNode("Cell 1");
-        tableData1.AddChild(dataText1);
-        tableRow1.AddChild(tableData1);
-
-        LightElementNode tableData2 = new LightElementNode("td", "inline", "closing", new List<string>());
-        LightTextNode dataText2 = new LightTextNode("Cell 2");
-        tableData2.AddChild(dataText2);
-        tableRow1.AddChild(tableData2);
-
-        header.RunLifecycleHooks();
-        Console.WriteLine(header.GetOuterHtml());
-        table.RunLifecycleHooks();
-        Console.WriteLine(table.GetOuterHtml());
+        // Додавання форми з текстовим полем
+        LightTextInputNode inputField = new LightTextInputNode("username");
+        header.AddChild(inputField);
+        // Додавання кнопки
+        LightButtonNode submitButton = new LightButtonNode("Submit");
+        header.AddChild(submitButton);
+        // Додавання випадаючого списку
+        List<string> options = new List<string> { "Option 1", "Option 2", "Option 3" };
+        LightSelectNode selectList = new LightSelectNode(options);
+        header.AddChild(selectList);
+        // Використовуємо відвідувача для обходу дерева HTML та виводу
+        IVisitor htmlVisitor = new HtmlVisitor();
+        header.Accept(htmlVisitor);
     }
 }
